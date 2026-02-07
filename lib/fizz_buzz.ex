21490ccccecc1c28615d6a6d8af99175544cfe0c
Sam Marxz <sammarxzz@gmail.com>
2026-02-07 13:21:56 -0300

    evaluate_numbers

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -8,20 +8,32 @@ defmodule FizzBuzz do
   defp read_file(file_name) do
     case File.read(file_name) do
       {:ok, content} -> content
-      {:error, reason} -> {:error, "File Error: #{reason}"}
+      {:error, _reason} -> {:error, "Arquivo não encontrado!"}
     end
   end
 
   defp parse_content({:error, _} = error), do: error
   defp parse_content(content) do
-    content
-    |> String.split(",")
-    |> Enum.map(&convert_and_trim/1)
+    result =
+      content
+      |> String.split(",", trim: true)
+      |> Enum.map(&process_item/1)
+
+    {:ok, result}
   end
 
-  defp convert_and_trim(item) do
+  defp process_item(item) do
     item
     |> String.trim()
-    |> String.to_integer()
+    |> Integer.parse()
+    |> handle_parse()
   end
+
+  defp handle_parse({number, _rest}), do: evaluate_numbers(number)
+  defp handle_parse(:error), do: :invalid_number
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizz_buzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
